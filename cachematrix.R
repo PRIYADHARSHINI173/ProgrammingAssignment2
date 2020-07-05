19acf6903c4162b83b032f325d36760039e19289
PRIYADHARSHINI173 <67816032+PRIYADHARSHINI173@users.noreply.github.com>
2020-07-05 12:37:12 +0530

    line 14

--- cachematrix.R
+++ cachematrix.R
@@ -11,7 +11,7 @@ makeCacheMatrix <- function(x = matrix()) {
 	}
 	get <- function() {x}
 	setInverse <- function(inverse) {inv <<- inverse}
-	getinverse <- function() {inv}
+	getInverse <- function() {inv}
 	list(set =set , get = get , setInverse = setInverse , getInverse = getInverse)
 }
 
